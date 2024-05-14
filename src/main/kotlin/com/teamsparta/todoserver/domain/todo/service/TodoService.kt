52acc5b54398da2d536d5c02734de115cac2b483
HR <dev.muun07@gmail.com>
2024-05-14 19:45:40 +0900

    ✨[Feat] Todo Service 추가

---
+++ src/main/kotlin/com/teamsparta/todoserver/domain/todo/service/TodoService.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.todoserver.domain.todo.service
+
+import com.teamsparta.todoserver.domain.todo.dto.CreateTodoRequest
+import com.teamsparta.todoserver.domain.todo.dto.TodoResponse
+import com.teamsparta.todoserver.domain.todo.dto.UpdateTodoRequest
+
+interface TodoService {
+    fun getAllTodos():List<TodoResponse>
+    fun getTodoById(todoId:Long): TodoResponse
+    fun createTodo(createTodoRequest: CreateTodoRequest): TodoResponse
+    fun updateTodo(todoId:Long, updateTodoRequest: UpdateTodoRequest): TodoResponse
+    fun deleteTodo(todoId:Long)
+}
---
+++ src/main/kotlin/com/teamsparta/todoserver/domain/todo/service/TodoServiceImpl.kt
@@ -0,0 +1,27 @@
+package com.teamsparta.todoserver.domain.todo.service
+
+import com.teamsparta.todoserver.domain.todo.dto.CreateTodoRequest
+import com.teamsparta.todoserver.domain.todo.dto.TodoResponse
+import com.teamsparta.todoserver.domain.todo.dto.UpdateTodoRequest
+
+class TodoServiceImpl :TodoService{
+    override fun getAllTodos(): List<TodoResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTodoById(todoId: Long): TodoResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun createTodo(createTodoRequest: CreateTodoRequest): TodoResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTodo(todoId: Long, updateTodoRequest: UpdateTodoRequest): TodoResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTodo(todoId: Long) {
+        TODO("Not yet implemented")
+    }
+}
