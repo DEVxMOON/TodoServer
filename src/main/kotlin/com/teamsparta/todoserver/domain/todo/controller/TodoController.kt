4ca5f259499798d88b0e7e98389fd097e4f6e020
HR <dev.muun07@gmail.com>
2024-05-14 19:57:50 +0900

    ✨[Feat] Todo Controller와 Service 연결

--- src/main/kotlin/com/teamsparta/todoserver/domain/todo/controller/TodoController.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/todo/controller/TodoController.kt
@@ -3,37 +3,49 @@ package com.teamsparta.todoserver.domain.todo.controller
 import com.teamsparta.todoserver.domain.todo.dto.CreateTodoRequest
 import com.teamsparta.todoserver.domain.todo.dto.TodoResponse
 import com.teamsparta.todoserver.domain.todo.dto.UpdateTodoRequest
+import com.teamsparta.todoserver.domain.todo.service.TodoService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
 @RequestMapping("/todos")
 @RestController
-class TodoController {
+class TodoController (private val todoService: TodoService) {
 
     @GetMapping
     fun getTodoList(): ResponseEntity<List<TodoResponse>> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.getAllTodos())
     }
 
     @GetMapping("/{todoId}")
-    fun getTodoById(@PathVariable todoId: Int) : ResponseEntity<TodoResponse> {
-        TODO()
+    fun getTodoById(@PathVariable todoId: Long) : ResponseEntity<TodoResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.getTodoById(todoId))
     }
 
     @PostMapping
     fun createTodo(@RequestBody createTodoRequest: CreateTodoRequest):ResponseEntity<TodoResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(todoService.createTodo(createTodoRequest))
     }
 
     @PutMapping("/{todoId}")
-    fun updateTodo(@PathVariable todoId: Int, @RequestBody updateTodoRequest: UpdateTodoRequest): ResponseEntity<TodoResponse> {
-        TODO()
+    fun updateTodo(@PathVariable todoId: Long, @RequestBody updateTodoRequest: UpdateTodoRequest): ResponseEntity<TodoResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.updateTodo(todoId, updateTodoRequest))
     }
 
     @DeleteMapping("/{todoId}")
-    fun deleteTodo(@PathVariable todoId: Int) : ResponseEntity<TodoResponse> {
-        TODO()
+    fun deleteTodo(@PathVariable todoId: Long) : ResponseEntity<TodoResponse> {
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
     }
 
 }
