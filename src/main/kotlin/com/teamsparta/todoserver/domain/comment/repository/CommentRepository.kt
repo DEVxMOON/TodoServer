be9a570ff505630548d19379e1474f40df2d145a
HR <dev.muun07@gmail.com>
2024-05-15 23:14:09 +0900

    ✨[Feat] Comment Repository 생성

---
+++ src/main/kotlin/com/teamsparta/todoserver/domain/comment/repository/CommentRepository.kt
@@ -0,0 +1,7 @@
+package com.teamsparta.todoserver.domain.comment.repository
+
+import com.teamsparta.todoserver.domain.comment.model.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CommentRepository  : JpaRepository<Comment, Long> {
+}
