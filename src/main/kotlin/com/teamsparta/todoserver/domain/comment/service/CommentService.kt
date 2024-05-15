c0f85469d46837b43f849079df15196cdeb0e527
HR <dev.muun07@gmail.com>
2024-05-15 22:02:09 +0900

    ✨[Feat] Comment Service 인터페이스 함수 추가

--- src/main/kotlin/com/teamsparta/todoserver/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/comment/service/CommentService.kt
@@ -1,6 +1,11 @@
 package com.teamsparta.todoserver.domain.comment.service
 
+import com.teamsparta.todoserver.domain.comment.dto.CommentResponse
+import com.teamsparta.todoserver.domain.comment.dto.CreateCommentRequest
+import com.teamsparta.todoserver.domain.comment.dto.UpdateCommentRequest
+
 interface CommentService {
-
-
+    fun createComment(todoId:Long,createCommentRequest: CreateCommentRequest) : CommentResponse
+    fun updateComment(todoId:Long, commentId:Long, updateCommentRequest: UpdateCommentRequest, name:String, password:String) : CommentResponse
+    fun deleteComment(todoId:Long,commentId:Long,name:String,password:String)
 }
