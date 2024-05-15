0705940310750bcf9483fe2edf9fd9d1e2494128
HR <dev.muun07@gmail.com>
2024-05-15 23:32:07 +0900

    ✨[Feat] Comment Service와 Controller 연결

--- src/main/kotlin/com/teamsparta/todoserver/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/comment/controller/CommentController.kt
@@ -3,19 +3,20 @@ package com.teamsparta.todoserver.domain.comment.controller
 import com.teamsparta.todoserver.domain.comment.dto.CommentResponse
 import com.teamsparta.todoserver.domain.comment.dto.CreateCommentRequest
 import com.teamsparta.todoserver.domain.comment.dto.UpdateCommentRequest
+import com.teamsparta.todoserver.domain.comment.service.CommentService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/todos/{todoId}/comments")
 @RestController
-class CommentController {
+class CommentController(private val commentService: CommentService) {
 
     @PostMapping
     fun createComment(@PathVariable todoId:Long, @RequestBody createComment: CreateCommentRequest): ResponseEntity<CommentResponse> {
         return ResponseEntity
             .status(HttpStatus.CREATED)
-            .build()
+            .body(commentService.createComment(todoId, createComment))
 
     }
 
@@ -23,13 +24,13 @@ class CommentController {
     fun updateComment(@PathVariable todoId:Long,@PathVariable commentId:Long, name:String,password:String, @RequestBody updateComment: UpdateCommentRequest): ResponseEntity<CommentResponse> {
         return ResponseEntity
             .status(HttpStatus.OK)
-            .build()
+            .body(commentService.updateComment(todoId, commentId, updateComment,name,password))
     }
 
     @DeleteMapping("/{commentId}")
     fun deleteComment(@PathVariable todoId:Long,@PathVariable commentId: Long,name:String,password:String): ResponseEntity<Unit> {
         return ResponseEntity
             .status(HttpStatus.NO_CONTENT)
-            .build()
+            .body(commentService.deleteComment(todoId,commentId,name,password))
     }
 }
