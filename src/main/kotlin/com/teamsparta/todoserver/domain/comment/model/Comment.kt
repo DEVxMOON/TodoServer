5150f893e2ebbe0dff643bfe81e1f2cd7f080d6e
HR <dev.muun07@gmail.com>
2024-05-15 23:13:46 +0900

    ✨[Feat] Comment Entity 생성

---
+++ src/main/kotlin/com/teamsparta/todoserver/domain/comment/model/Comment.kt
@@ -0,0 +1,32 @@
+package com.teamsparta.todoserver.domain.comment.model
+
+import com.teamsparta.todoserver.domain.comment.dto.CommentResponse
+import com.teamsparta.todoserver.domain.todo.model.Todo
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "comments")
+class Comment(
+    @Column(name = "name", nullable = false)
+    var name: String,
+
+    @Column(name = "password", nullable = false)
+    var password: String,
+
+    @Column(name ="body")
+    var body: String,
+
+    ){
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long?=null
+}
+
+fun Comment.toResponse(): CommentResponse {
+    return CommentResponse(
+        id=id!!,
+        name=name,
+        body = body,
+    )
+}
