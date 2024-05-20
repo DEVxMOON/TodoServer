94bd0437db18d6444a0dbdc567a0d87c1ce2fb47
HR <dev.muun07@gmail.com>
2024-05-20 12:55:57 +0900

    ♻️[Refactor] EntityGraph를 활용한 매칭

--- src/main/kotlin/com/teamsparta/todoserver/domain/todo/repository/TodoRepository.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/todo/repository/TodoRepository.kt
@@ -1,8 +1,12 @@
 package com.teamsparta.todoserver.domain.todo.repository
 
 import com.teamsparta.todoserver.domain.todo.model.Todo
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface TodoRepository : JpaRepository<Todo, Long> {
-
+    @EntityGraph(attributePaths = ["comments"])
+    @Query("SELECT t FROM Todo t")
+    fun findAllWithComments():List<Todo>
 }
--- src/main/kotlin/com/teamsparta/todoserver/domain/todo/service/TodoServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/todo/service/TodoServiceImpl.kt
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class TodoServiceImpl(private val todoRepository: TodoRepository) :TodoService{
     override fun getAllTodos(): List<TodoResponse> {
-        return todoRepository.findAll().map { it.toResponse() }
+        return todoRepository.findAllWithComments().map { it.toResponse() }
     }
 
     override fun getTodoById(todoId: Long): TodoResponse {
