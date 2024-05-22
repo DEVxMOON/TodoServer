a494ff2f4f5495b7dd3877d13e14f1c37b653256
HR <dev.muun07@gmail.com>
2024-05-22 12:23:32 +0900

    ✨[Feat] 회원가입 조건 추가

--- src/main/kotlin/com/teamsparta/todoserver/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/member/repository/MemberRepository.kt
@@ -4,4 +4,6 @@ import com.teamsparta.todoserver.domain.member.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository :JpaRepository<Member,Long>{
+    fun findMemberByName(memberName : String) : Member?
+    fun findMemberByLoginId(loginId : String) : Member?
 }
--- src/main/kotlin/com/teamsparta/todoserver/domain/member/service/MemberServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todoserver/domain/member/service/MemberServiceImpl.kt
@@ -10,7 +10,14 @@ import org.springframework.stereotype.Service
 @Service
 class MemberServiceImpl(private val memberRepository : MemberRepository) : MemberService  {
     override fun signUp(memberRequest: MemberRequest): MemberResponse {
-        //TODO : DB에 아이디, 이름이 동일한 사람이 있는지 확인
+
+        //TODO : 조건 조금 더 고민, 아이디가 핵심? 작성자 이름 기준으로 댓글, TODO를 작성한다고 하면. HOW?
+        if(memberRepository.findMemberByName(memberRequest.name) != null
+            && memberRepository.findMemberByLoginId(memberRequest.loginId) != null){
+            throw IllegalArgumentException("LoginId Already Exist")
+        }
+
+        //TODO : Password 암호화를 해야할 것 같은데... signUp을 할 때, token을 발행해서 넣어줘야할 것 같다.
         return memberRepository.save(
             Member(
                 loginId = memberRequest.loginId,
